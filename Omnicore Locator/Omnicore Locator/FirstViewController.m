f77b3d7f6970cf5b5a1d836aee15859fcd97c88f
Johan Karlemo <johankarlemo@Omnicore-MBA.local>
2012-07-10 11:45:33 +0200

    First commit

---
+++ Omnicore Locator/Omnicore Locator/FirstViewController.m
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.m
+//  Omnicore Locator
+//
+//  Created by Johan Karlemo on 2012-07-10.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ Omnicore Locator/Omnicore Locator/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Omnicore Locator
+//
+//  Created by Johan Karlemo on 2012-07-10.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
